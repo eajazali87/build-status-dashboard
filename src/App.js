857a4c6a51f5097584d58bec7e0feb1437c80d45
Mahaboob Basha, Eajaz Ali <EajazAli.MahaboobBasha@pearson.com>
2019-07-02 11:47:40 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import CORS from 'cors'
+import './App.css'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor () {
+  super()
+  this.state = {
+  autobahn_result: '',
+  ascend_result: '',
 }
 
-export default App;
+}
+
+componentDidMount()   {
+  axios.get('https://jenkins-skynet.pla.pearsondev.tech/view/Autobahn-All-In-One/job/Autobahn-All-In-One/lastBuild/api/json',
+  {
+    headers: {
+      "Accept":"application/json",
+      "Authorization":"Basic dW1haGFlYTpQYXNzd29yZEAx",
+    }
+  }).then(response => this.setState({ autobahn_result: response.data.result }))
+
+  axios.get('https://jenkins-skynet.pla.pearsondev.tech/view/All-In-One/job/All-In-One/lastBuild/api/json',
+    {
+      headers: {
+        "Accept":"application/json",
+        "Authorization":"Basic dW1haGFlYTpQYXNzd29yZEAx",
+      }
+  }).then(response => this.setState({ ascend_result: response.data.result }))
+}
+
+autobahnStyles = () => {
+  if (this.state.autobahn_result === 'SUCCESS') {
+    return 'success'
+  }
+  if (this.state.autobahn_result === 'FAILURE') {
+    return 'failure'
+  }
+
+  return 'other'
+}
+
+ascendStyles = () => {
+  if (this.state.ascend_result === 'SUCCESS') {
+    return 'success'
+  }
+  if (this.state.ascend_result === 'FAILURE') {
+    return 'failure'
+  }
+
+  return 'other'
+}
+
+  render () {
+    return (
+      <div className='button__container'>
+      <h1>Project Skynet Execution Status</h1>
+
+      <table>
+        <tr>
+          <th>Team</th>
+          <th>Execution Status</th>
+        </tr>
+        <tr>
+          <td>Autobahn</td>
+          <td className={`autobahn_text_${this.autobahnStyles()}`}>{this.state.autobahn_result}</td>
+        </tr>
+        <tr>
+          <td>Ascend</td>
+          <td className={`ascend_text_${this.ascendStyles()}`}>{this.state.ascend_result}</td>
+        </tr>
+      </table>
+      </div>
+
+    )
+
+
+  }
+}
+export default App
